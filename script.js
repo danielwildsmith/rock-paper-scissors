dc3c6cbd586565bce9d40dc8bcaebe1120137970
Daniel <danielwildsmith@ufl.edu>
2022-07-10 16:00:35 -0400

    Create computer select function

---
+++ script.js
@@ -0,0 +1,13 @@
+function computerPlay() {
+    let randomNumber = Math.floor(Math.random() * 3);
+    
+    if(randomNumber == 0) {
+        return 'Rock';
+    } else if(randomNumber == 1) {
+        return 'Paper';
+    } else {
+        return 'Scissors';
+    }
+}
+
+console.log(computerPlay())
